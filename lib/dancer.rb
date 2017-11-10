57d498783722154e5e5d1af337abc9ecde6aa884
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-10 01:43:37 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,11 +1,11 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
-class Kid
+class Dancer
   include Dance
-
+ 
   attr_accessor :name
-
+ 
   def initialize(name)
     @name = name
   end
